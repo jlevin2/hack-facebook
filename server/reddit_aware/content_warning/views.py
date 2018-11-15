e22e7572e2a2b5f3e5bc757e56155ac44d587933
Josh Levin <joshua_levin2@brown.edu>
2018-11-15 15:18:36 -0800

    django basic model working

--- server/reddit_aware/content_warning/views.py
+++ server/reddit_aware/content_warning/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Document
 # Create your views here.
 
-
 def response(request):
-    return HttpResponse("Content_warning?")
+    page = request.GET.get('page')
+    inDB = checkDB(page)
+    return HttpResponse(str(inDB))
+    # d = Document.objects.create(page_id='test_page', trigger_content='sex', probability=1.00)
+    # d.save()
+    # return HttpResponse("PUT THE OBJECT IN!")
+
+
+def checkDB(page):
+    contents = Document.objects.filter(page_id=page)
+    if len(contents) == 0:
+        return 0
+        # run model
+    else:
+        return 1
