1a3a71e00df56ea59c68a3adf98212568e4327c7
Josh Levin <joshua_levin2@brown.edu>
2018-11-16 00:50:05 -0800

    server update keyword and working

--- server/reddit_aware/content_warning/ml.py
+++ server/reddit_aware/content_warning/ml.py
@@ -9,11 +9,26 @@ def runModel(cont, typ):
         return classifyUrl(cont)
 
 def classifyText(text):
-    return [False, False, True, True]
+    categories = open('/Users/JoshLevin/Desktop/hack@facebook/hack-facebook/keywords.txt', 'r')
+    keywords = categories.readlines()
+    res = [False, False, False, False]
+    for i,c in enumerate(keywords):
+        for w in c.split(','):
+
+            if w in text:
+                res[i] = True
+    #
+    # for w in banned:
+    #     if w in text:
+    #         return [True, True, True, True]
+
+    return res
+   # return [False, False, True, True]
 
 
 def classifyUrl(url):
     g = requests.get(url)
+    #print(g.text)
     return classifyText(g.text)
 
 
